1107cf9ea4f08c77a915da7047ec7ba3244f56fd
Cal Heldenbrand <cal@fbsdata.com>
2013-08-15 14:13:24 -0500

    Updating configuration.py to generate the header for use with watch faces

--- configuration.py
+++ configuration.py
@@ -24,6 +24,14 @@ def genKeyLine( code ):
   key_hex = ["0x%02X" % x for x in key_bytes]
   return "{ " + ', '.join(key_hex) + " },"
 
+def genAppId(key):
+  # Start with 8 bytes of a static UUID
+  ar = [ "0xA4", "0xA6", "0x13", "0xB5", "0x8A", "0x6B", "0x4F", "0xF0"]
+  # Append 8 more bytes of the given key
+  for i in xrange(2, 45, 6):
+    ar.append( key[ i : i+4] )
+  return ", ".join(ar)
+
 try:
   f = open( 'configuration.txt','r' )
 except:
@@ -42,9 +50,12 @@ for line in f:
 f.close()
 
 f = open( "src/configuration.h","w" )
+#f = sys.stderr
 f.write( "#ifndef _CONFIGURATION_H_\n#define _CONFIGURATION_H_\n" )
 f.write( "#define NUM_SECRETS %i\n" % len(labels) )
 f.write( "#define DEFAULT_TIME_ZONE %s\n" % time_zone )
+f.write( "#define APP_NAME \"Authenticator %s\"\n" % labels[0])
+f.write( "#define MY_UUID { %s }\n" % genAppId( secrets[0]) )
 f.write( "char otplabels[NUM_SECRETS][10] = {\n    " )
 for label in labels:
   f.write( "\"%s\"," % label )
